4a299b2bf505036e5f20f36272301ae3c2e79707
dhruvenvora <dhruven91@gmail.com>
2016-03-18 18:36:38 -0700

    all reset

--- home/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-class UserActivity(models.Model):
-    userid = models.CharField(max_length = 16, primary_key=True) #primary key
-    login_time = models.CharField(max_length = 100)
